4ecb13b54f5211b9c8a8573a3b2f160a11b924a9
subhumardi07 <subhashmardi8@gamil.com>
2024-12-10 15:21:06 +0530

    previous and next button
    previous and next button woking till last page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/Textform';
-// import About from './components/About'; 
+import About from './components/About'; 
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes,
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 
 
@@ -42,23 +42,23 @@ function App() {
   }
   return (
     <>
-    {/* <Router> */}
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
     <Alert alert={alert}/>
     <div className="container my-3">
-    {/* <Routes> */}
+    <Routes> 
     {/* /users --> Component 1
         /users/home --> Component 2 */}
-          {/* <Route exact path="/about">
+          <Route exact path="/about">
             <About mode={mode} />
-          </Route> */}
-          {/* <Route exact path="/"> */}
+          </Route>
+            <Route exact path="/"> 
             <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
-          {/* </Route> */}
+          </Route>
 
-    {/* </Routes> */}
+    </Routes>
     </div>
-    {/* </Router> */}
+    </Router>
 
     </> 
   );
